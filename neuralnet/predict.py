58cfe664d9348d2fcb684e2da81f04f0f60e81e0
pshah123 <priansh123@me.com>
2017-06-06 19:15:14 -0400

    Added flask website testing for CMU dataset

--- neuralnet/predict.py
+++ neuralnet/predict.py
@@ -5,12 +5,11 @@ import os
 from sys import argv
 usage = "\nUsage:\npython main.py path/to/dataset.csv #MAX_GPA #MAX_TEST_SCORE\n\nExample:\tpython main.py harvard.csv 6.0 2400\n\nThe dataset should have one column of GPA and one column of applicable test scores, no headers."
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-try:
-    script, dataset_filename, maxgpa, maxtest = argv
-except:
-    print("An error has occurred: "+str(sys.exc_info()[0]))
-    print(usage)
-    exit(0)
+
+script='predict.py'
+dataset_filename='./neuralnet/corpus/carnegie_mellon.csv'
+maxgpa=5.0
+maxtest=2400
 dataset_filename = str(dataset_filename)
 maxgpa = float(maxgpa)
 maxtest = int(maxtest)
@@ -36,7 +35,7 @@ feature_columns = [tf.contrib.layers.real_valued_column("", dimension=2)]
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10, 20, 10],
                                             n_classes=3,
-                                            model_dir="./train/model/"+dataset_filename[dataset_filename.rfind('/')+1:-4],
+                                            model_dir="./neuralnet/train/model/"+dataset_filename[dataset_filename.rfind('/')+1:-4],
                                             config=tf.contrib.learn.RunConfig(
                                                 save_checkpoints_secs=60))
 
@@ -50,29 +49,15 @@ def get_test_inputs():
     y = tf.constant(test_set.target)
     return x, y
 
-print("How many steps should we train for?")
-maxsteps = int(input('> '))
+maxsteps=1
 # Create the classifier. Take maxsteps steps.
 classifier.fit(input_fn=get_train_inputs, steps=maxsteps)
-
-# Evaluate accuracy.
-accuracy_score = classifier.evaluate(input_fn=get_test_inputs, steps=1)["accuracy"]
-print('\nFinished with accuracy {0:f}'.format(accuracy_score))
-
-print("Please provide a GPA and test score to chance.")
-cur_gpa = float(input('GPA: '))
-test_score = int(input('Test Score: '))
-def new_samples():
-    return np.array([[0.0, 0], [cur_gpa,test_score], [maxgpa, maxtest]], dtype=np.float32)
-predictions = list(classifier.predict(input_fn=new_samples))
-print("Made predictions.")
-
-def returnChance(chance):
-    if chance==0:
-        return "rejection"
-    if chance==1:
-        return "admission"
-
-print("For GPA 0 and test score 0, the result will be "+returnChance(predictions[0]))
-print("For GPA "+str(cur_gpa)+" and test score "+str(test_score)+", the result will be "+returnChance(predictions[1]))
-print("For GPA "+str(maxgpa)+" and test score "+str(maxtest)+", the result will be "+returnChance(predictions[2]))
+def predict(cur_gpa, testscore,test_type):
+    gpa_in=0
+    testscore_in =0
+    gpa_in = cur_gpa
+    testscore_in = testscore
+    def new_samples():
+        return np.array([[gpa_in,testscore_in]], dtype=np.float32)
+    predictions = list(classifier.predict(input_fn=new_samples))
+    return predictions
---
+++ website.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import neuralnet.predict as pr
+app=Flask(__name__)
+@app.route('/predict', methods=['POST'])
+def predict():
+    gpa = request.form["gpa"]
+    score = request.form["test_score"]
+    predictions=[]
+    score=int(score)
+    gpa=float(gpa)
+    print(str(gpa)+", "+str(score))
+    if score>36:
+        predictions=pr.predict(gpa,score,"ACT")
+    if score<=1600:
+        predictions=pr.predict(gpa,score,"SAT1600")
+    else:
+        predictions = pr.predict(gpa,score,"SAT2400")
+    if predictions[0]==1:
+        return "Congratulations, you got in!"
+    else:
+        if predictions[0]==0:
+            return "Sorry, rejected."
+        return "Something went wrong."
+
+@app.route('/')
+def home():
+    return render_template('website.html')
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run(host='0.0.0.0')
