cca3afc98f6041ff986c13fd56a811516426f165
Priansh Shah <shah@priansh.com>
2017-10-12 00:52:45 +0100

    better templating

--- website.py
+++ website.py
@@ -1,30 +1,36 @@
 from flask import Flask, render_template, request
 import neuralnet.predict as pr
+from console_logging.console import Console
+console = Console()
 app=Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
-    gpa = request.form["gpa"]
-    score = request.form["test_score"]
+
+    # get form variables and type them
+    gpa = float(request.form["gpa"])
+    score = int(request.form["test_score"])
+    console.info("Chancing GPA: %d, SAT: %d"%(gpa,score))
     predictions=[]
-    score=int(score)
-    gpa=float(gpa)
-    print(str(gpa)+", "+str(score))
-    if score>36:
+
+    #TODO: implement test type. This is a stub.
+    if score<=36:
         predictions=pr.predict(gpa,score,"ACT")
-    if score<=1600:
+    elif score<=1600:
         predictions=pr.predict(gpa,score,"SAT1600")
     else:
         predictions = pr.predict(gpa,score,"SAT2400")
+    ##
+
     if predictions[0]==1:
-        return "I would recommend you apply as a match/safety."
+        return "Admission is likely."
     else:
         if predictions[0]==0:
-            return "This would work as a high reach."
+            return "Admission is unlikely."
         return "Something went wrong."
 
 @app.route('/')
 def home():
-    return render_template('website.html')
+    return render_template('website.html', college={'name':'CMU','accuracy':'76.4045'})
 
 if __name__ == '__main__':
     app.debug=True
